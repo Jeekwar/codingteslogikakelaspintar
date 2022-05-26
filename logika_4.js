6a99f3347bc85b44069639e474da599d0b8dcc45
jee <79134153+Jeekwar@users.noreply.github.com>
2022-05-27 04:52:13 +0700

    Create logika_4.js

---
+++ logika_4.js
@@ -0,0 +1,17 @@
+const MyFunc()=>{
+let n =[];
+for(let i=0; i<=n; i++) {
+    let target = n;
+    while (target < n && target != array[target]){
+        new_target = array[target];
+        array[target] = target;
+        target = new_target;
+    }
+        
+}
+for (i-0; i<=n){
+        if (array[i] != i){
+            return i}
+    return n;
+}
+}
